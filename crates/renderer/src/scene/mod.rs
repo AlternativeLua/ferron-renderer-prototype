8eac21276d74dfac3f892e612393586b958b85c4
Richard Vitus <alternative@Mac.fritz.box>
2026-06-26 11:32:34 +0100

    ecs conversion completed

--- crates/renderer/src/scene/mod.rs
+++ crates/renderer/src/scene/mod.rs
@@ -1,28 +1,11 @@
 mod camera;
 mod components;
 mod mesh;
+mod time;
 mod transform;
-//mod time;
 
 pub use camera::Camera;
-pub use components::{LocalTransform, MaterialHandle, MeshHandle};
+pub use components::{LocalTransform, MaterialHandle, MeshHandle, Spin};
 pub use mesh::CpuMesh;
+pub use time::Time;
 pub use transform::Transform;
-
-pub struct RenderObject {
-    pub mesh: MeshHandle,
-    pub transform: Transform,
-}
-
-#[derive(Default)]
-pub struct Scene {
-    pub objects: Vec<RenderObject>,
-}
-
-impl Scene {
-    #[inline]
-    pub fn spawn(&mut self, mesh: MeshHandle, transform: Transform) -> &mut RenderObject {
-        self.objects.push(RenderObject { mesh, transform });
-        self.objects.last_mut().unwrap()
-    }
-}
